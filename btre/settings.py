72bf1e7fb6eedf7d250c3eaad2c8cbdf4a6b6d73
George Grisancich <george@rockposters.com.au>
2020-04-08 16:39:04 +1000

    listing and realtor models added

--- btre/settings.py
+++ btre/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'rockgis',
+        'USER': 'postgres',
+        'PASSWORD': 'kcor',
+        'HOST': 'localhost',
+        'PORT': '5432',   
     }
 }
 
